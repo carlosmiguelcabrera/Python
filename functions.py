d2e9e0e2da625c6d958538bfdcec0166fb909fe2
Carlos Miguel Cabrera <108997509+carlosmiguelcabrera@users.noreply.github.com>
2023-06-06 20:45:19 -0300

    Actualizar functions.py

--- functions.py
+++ functions.py
@@ -4,3 +4,17 @@ num1 = 10
 num2 = 5
 suma = num1 + num2
 print("La suma es:", suma)
+
+# Ejercicio 2: Factorial de un número
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+numero = 5
+resultado = factorial(numero)
+print("El factorial de", numero, "es", resultado)
+
+
