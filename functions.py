c6889fc3b9571eda629aa6fd36da10649b7f4483
Carlos Miguel Cabrera <108997509+carlosmiguelcabrera@users.noreply.github.com>
2023-06-08 20:30:52 -0300

    Actualizar functions.py

--- functions.py
+++ functions.py
@@ -86,3 +86,20 @@ promedio = sum(numeros) / len(numeros)
 print("El promedio es:", promedio)
 
 
+#Números primos: Muestra todos los números primos en un rango dado.
+
+def es_primo(numero):
+    if numero < 2:
+        return False
+    for i in range(2, int(numero**0.5) + 1):
+        if numero % i == 0:
+            return False
+    return True
+
+inicio = int(input("Ingrese el inicio del rango: "))
+fin = int(input("Ingrese el fin del rango: "))
+
+print("Números primos:")
+for num in range(inicio, fin + 1):
+    if es_primo(num):
+        print(num)
